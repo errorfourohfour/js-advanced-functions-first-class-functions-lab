fb1e6afe7cce3ba0c64c1eff9c70ff80fddccda2
Zach Brookman <zachbrookman48@gmail.com>
2021-04-28 16:44:53 -0400

    commit tree 53081b5d81cd55c62d029a8a7ee6d798c01dbd8b

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+const drivers2 = ['Char', 'Mike', 'Chad', 'Zach']
+const returnFirstTwoDrivers = function (driversSelection){
+    const a = driversSelection[0];
+    const b = driversSelection[1];
+    return [a,b]
+    // const Freddy = 
+    // return drivers[1,2]
+}
+
+const returnLastTwoDrivers = function () {
+         let myArray = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+         return myArray.slice(-2)
+     }
+     
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+ 
+ function createFareMultiplier(integer){
+
+     function fareMultiplier(fare){
+     return integer*fare
+ }
+ return fareMultiplier;
+}
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(arrayOfDrivers, selectDemDrivers){
+    return  selectDemDrivers(arrayOfDrivers)
+}
